a3b9e8f89c9ed0dab650ad708574601c1e89a96c
Chris Nuernberger <chris@techascent.com>
2020-10-26 07:17:14 -0600

    Adding in what native test code I have so far

--- src/simpledata/main.clj
+++ src/simpledata/main.clj
@@ -1,15 +1,25 @@
 (ns simpledata.main
-  (:require [tech.v3.dataset :as ds]
+  (:require [tech.v3.libs.arrow.in-place :as in-place]
+            ;;need datafy defininitions
+            [tech.v3.libs.arrow.schema]
             [tech.v3.datatype.functional :as dfn])
   (:gen-class))
 
 
+(comment
+
+  (do
+    (defn- flights->arrow
+      []
+      (-> (ds/->dataset "flights14.csv")
+          (arrow/write-dataset-to-stream! "flights14.arrow"))))
+  )
 
 (defn -main
   [& args]
-  (let [flights
-        (ds/->dataset "flights14.csv")]
-    (println "Number of flights whose average arrival and departure delay was less than zero:"
+  (System/load "/home/chrisn/dev/cnuernber/simpledata/ld-libs/liblarray.so")
+  (let [flights (in-place/read-stream-dataset-inplace "flights14.arrow")]
+    (println "Number of flights whose sum arrival and departure delay was less than zero:"
              (-> (dfn/+ (flights "arr_delay")
                         (flights "dep_delay"))
                  (dfn/< 0)
